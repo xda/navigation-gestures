6dff3b654d408d6708e9aa29d56baae4ed836eb0
zacharee <zachary.wander@gmail.com>
2018-05-29 21:36:03 -0400

    Hopefully avoid crash

--- app/src/main/java/com/xda/nobar/App.kt
+++ app/src/main/java/com/xda/nobar/App.kt
@@ -463,28 +463,30 @@ class App : Application(), SharedPreferences.OnSharedPreferenceChangeListener {
         }
 
         override fun onReceive(context: Context?, intent: Intent?) {
-            when (intent?.action) {
-                Intent.ACTION_SCREEN_ON -> {
-                    handler.postDelayed({
-                        if (isActivated() && (kgm.isKeyguardLocked || if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) kgm.isDeviceLocked else false)) {
+            if (!IntroActivity.needsToRun(this@App)) {
+                when (intent?.action) {
+                    Intent.ACTION_SCREEN_ON -> {
+                        handler.postDelayed({
+                            if (isActivated() && (kgm.isKeyguardLocked || if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) kgm.isDeviceLocked else false)) {
 //                        Log.e("NoBar", "on KeyGuard")
-                            toggle(true)
-                            Utils.setOffForRebootOrScreenLock(this@App, true)
-                        } else if (Utils.isOffForRebootOrScreenLock(this@App)) {
+                                toggle(true)
+                                Utils.setOffForRebootOrScreenLock(this@App, true)
+                            } else if (Utils.isOffForRebootOrScreenLock(this@App)) {
+                                toggle(false)
+                                Utils.setOffForRebootOrScreenLock(this@App, false)
+                            }
+                        }, 100)
+                    }
+                    Intent.ACTION_USER_PRESENT -> {
+                        if (isActivated()) addBar()
+                        if (Utils.isOffForRebootOrScreenLock(this@App)) {
                             toggle(false)
                             Utils.setOffForRebootOrScreenLock(this@App, false)
                         }
-                    }, 100)
-                }
-                Intent.ACTION_USER_PRESENT -> {
-                    if (isActivated()) addBar()
-                    if (Utils.isOffForRebootOrScreenLock(this@App)) {
-                        toggle(false)
-                        Utils.setOffForRebootOrScreenLock(this@App, false)
                     }
-                }
-                TOGGLE -> {
-                    toggle()
+                    TOGGLE -> {
+                        toggle()
+                    }
                 }
             }
         }
