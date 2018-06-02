9218672b77fa2b6788cbed5cca2339ff31b63349
zacharee <zachary.wander@gmail.com>
2018-06-01 20:41:32 -0400

    Improve Settings layout

--- app/src/main/java/com/xda/nobar/activities/SettingsActivity.kt
+++ app/src/main/java/com/xda/nobar/activities/SettingsActivity.kt
@@ -275,54 +275,21 @@ class SettingsActivity : AppCompatActivity() {
             val pillCornerRadius = findPreference("pill_corner_radius") as SliderPreferenceEmbedded
             val posX = findPreference("custom_x") as SliderPreferenceEmbedded
 
-            val resetW = findPreference("reset_width")
-            val resetH = findPreference("reset_height")
-            val resetY = findPreference("reset_pos_y")
-            val resetPill = findPreference("reset_pill_bg")
-            val resetPillBorder = findPreference("reset_pill_fg")
-            val resetCorners = findPreference("reset_pill_corner_radius")
-            val resetX = findPreference("reset_pos_x")
-            val defaults = findPreference("defaults")
-
             width.seekBar.min = Utils.dpAsPx(activity, 10)
             height.seekBar.min = Utils.dpAsPx(activity, 5)
             posY.seekBar.min = 0
             posX.seekBar.min = -(Utils.getRealScreenSize(activity).x.toFloat() / 2f - Utils.getCustomWidth(activity).toFloat() / 2f).toInt()
 
+            width.seekBar.defaultProgress = resources.getDimensionPixelSize(R.dimen.pill_width)
+            height.seekBar.defaultProgress = resources.getDimensionPixelSize(R.dimen.pill_height)
+            posY.seekBar.defaultProgress = Utils.getDefaultY(activity)
+            posX.seekBar.defaultProgress = 0
+            pillCornerRadius.seekBar.defaultProgress = 8
+
             width.seekBar.max = screenSize.x
             height.seekBar.max = Utils.dpAsPx(activity, 50)
             posY.seekBar.max = Utils.dpAsPx(activity, 70)
             posX.seekBar.max = -posX.seekBar.min
-            
-            val resetListener = Preference.OnPreferenceClickListener {
-                preferenceManager.sharedPreferences.edit().apply {
-                    if (it.key == "reset_width" || it.key == "defaults") remove("custom_width")
-                    if (it.key == "reset_height" || it.key == "defaults") remove("custom_height")
-                    if (it.key == "reset_pos_y" || it.key == "defaults") remove("custom_y")
-                    if (it.key == "reset_pos_x" || it.key == "defaults") remove("custom_x")
-                    if (it.key == "reset_pill_bg" || it.key == "defaults") remove("pill_bg")
-                    if (it.key == "reset_pill_fg" || it.key == "defaults") remove("pill_fg")
-                    if (it.key == "reset_pill_corner_radius" || it.key == "defaults") remove("pill_corner_radius")
-                    apply()
-                }
-                width.seekBar.progress = Utils.getCustomWidth(activity)
-                height.seekBar.progress = Utils.getCustomHeightWithoutHitbox(activity)
-                posY.seekBar.progress = Utils.getHomeY(activity)
-                posX.seekBar.progress = Utils.getHomeX(activity)
-                pillCornerRadius.seekBar.progress = Utils.getPillCornerRadiusInDp(activity)
-                pillColor.saveValue(Utils.getPillBGColor(activity))
-                pillBorderColor.saveValue(Utils.getPillFGColor(activity))
-                true
-            }
-
-            resetW.onPreferenceClickListener = resetListener
-            resetH.onPreferenceClickListener = resetListener
-            resetY.onPreferenceClickListener = resetListener
-            resetX.onPreferenceClickListener = resetListener
-            resetPill.onPreferenceClickListener = resetListener
-            resetPillBorder.onPreferenceClickListener = resetListener
-            resetCorners.onPreferenceClickListener = resetListener
-            defaults.onPreferenceClickListener = resetListener
         }
     }
 
@@ -340,44 +307,6 @@ class SettingsActivity : AppCompatActivity() {
             super.onResume()
 
             activity.title = resources.getText(R.string.behavior)
-
-            setListeners()
-        }
-
-        private fun setListeners() {
-            val hold = findPreference("hold_time") as SliderPreferenceEmbedded
-            val vib = findPreference("vibration_duration") as SliderPreferenceEmbedded
-            val anim = findPreference("anim_duration") as SliderPreferenceEmbedded
-
-            val resetHold = findPreference("reset_hold_time")
-            val resetVib = findPreference("reset_vibration_duration")
-            val resetAnim = findPreference("reset_anim_duration")
-            val resetAll = findPreference("reset_all")
-
-            val listener = Preference.OnPreferenceClickListener {
-                preferenceManager.sharedPreferences.edit().apply {
-                    if (it.key == "reset_hold_time" || it.key == "reset_all") {
-                        remove("hold_time")
-                        hold.seekBar.resetProgress()
-                    }
-                    if (it.key == "reset_vibration_duration" || it.key == "reset_all") {
-                        remove("vibration_duration")
-                        vib.seekBar.resetProgress()
-                    }
-                    if (it.key == "reset_anim_duration" || it.key == "reset_all") {
-                        remove("anim_duration")
-                        anim.seekBar.resetProgress()
-                    }
-
-                    apply()
-                }
-                true
-            }
-
-            resetHold.onPreferenceClickListener = listener
-            resetVib.onPreferenceClickListener = listener
-            resetAnim.onPreferenceClickListener = listener
-            resetAll.onPreferenceClickListener = listener
         }
     }
 
