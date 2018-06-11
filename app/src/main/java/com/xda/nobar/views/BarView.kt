93654e80432c735195322d1cc324e5f1e222752d
zacharee <zachary.wander@gmail.com>
2018-06-11 16:01:09 -0400

    Fix crash

--- app/src/main/java/com/xda/nobar/views/BarView.kt
+++ app/src/main/java/com/xda/nobar/views/BarView.kt
@@ -108,8 +108,8 @@ class BarView : LinearLayout, SharedPreferences.OnSharedPreferenceChangeListener
         }
     var immersiveNav: Boolean
         get() {
-            val imm = Settings.Global.getString(context.contentResolver, Settings.Global.POLICY_CONTROL)
-            return imm != null && imm.contains("navigation")
+            val imm = Settings.Global.getString(context.contentResolver, Settings.Global.POLICY_CONTROL) ?: return false
+            return imm.contains("navigation")
         }
         set(value) {
             if (Utils.shouldUseOverscanMethod(context)) {
