db951ce1d19fdd825d47a49344c9f0cfe51fb6bd
zachary <zachary.wander@gmail.com>
2018-11-16 07:35:41 -0500

    Remove import

--- app/src/main/java/com/xda/nobar/activities/BaseAppSelectActivity.kt
+++ app/src/main/java/com/xda/nobar/activities/BaseAppSelectActivity.kt
@@ -10,7 +10,6 @@ import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.SearchView
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
