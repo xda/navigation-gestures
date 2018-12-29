bf1fabae01fec55b94967c66cad14dc1fafca80b
Zachary Wander <zachary.wander@gmail.com>
2018-12-29 14:14:37 -0500

    Tweak PixelDPSwitch

--- app/src/main/java/com/xda/nobar/prefs/PixelDPSwitch.kt
+++ app/src/main/java/com/xda/nobar/prefs/PixelDPSwitch.kt
@@ -7,35 +7,24 @@ import com.xda.nobar.R
 import java.util.*
 
 class PixelDPSwitch(context: Context, attributeSet: AttributeSet) : SwitchPreference(context, attributeSet) {
+    var dimensionType: String
+        get() = throw IllegalArgumentException("not a gettable value")
+        set(value) {
+            title = String.format(
+                    Locale.getDefault(),
+                    context.resources.getString(R.string.use_pixels),
+                    value.capitalize()
+            )
+            summary = String.format(
+                    Locale.getDefault(),
+                    context.resources.getString(R.string.use_pixels_desc),
+                    value.toLowerCase()
+            )
+        }
 
     init {
         val array = context.theme.obtainStyledAttributes(attributeSet, R.styleable.PixelDPSwitch, 0, 0)
-        val dim = array.getString(R.styleable.PixelDPSwitch_dimension_name)
+        dimensionType = array.getString(R.styleable.PixelDPSwitch_dimension_name)
                 ?: context.resources.getString(R.string.dimension)
-
-        title = String.format(
-                Locale.getDefault(),
-                context.resources.getString(R.string.use_pixels),
-                dim
-        )
-
-        summary = String.format(
-                Locale.getDefault(),
-                context.resources.getString(R.string.use_pixels_desc),
-                dim.toLowerCase()
-        )
-    }
-
-    fun setDimensionType(dim: String) {
-        title = String.format(
-                Locale.getDefault(),
-                context.resources.getString(R.string.use_pixels),
-                dim
-        )
-        summary = String.format(
-                Locale.getDefault(),
-                context.resources.getString(R.string.use_pixels_desc),
-                dim.toLowerCase()
-        )
     }
 }
