8636c8dfaeb83072879bd61beaf81a50871fcfc6
Zachary Wander <zachary.wander@gmail.com>
2019-01-04 10:19:53 -0500

    Move CollapsiblePreferenceCategory to library

--- app/src/main/java/com/xda/nobar/fragments/settings/BasePrefFragment.kt
+++ app/src/main/java/com/xda/nobar/fragments/settings/BasePrefFragment.kt
@@ -6,8 +6,8 @@ import androidx.annotation.CallSuper
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceScreen
 import androidx.recyclerview.widget.RecyclerView
-import com.xda.nobar.adapters.prefs.CollapsiblePreferenceGroupAdapter
 import com.xda.nobar.util.prefManager
+import tk.zwander.collapsiblepreferencecategory.CollapsiblePreferenceGroupAdapter
 
 abstract class BasePrefFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
     internal val prefManager by lazy { activity!!.prefManager }
