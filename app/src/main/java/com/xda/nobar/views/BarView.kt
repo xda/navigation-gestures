2d2e63bcc63eff00ecb55ef728629eb405718b0b
zacharee <zachary.wander@gmail.com>
2019-03-21 15:25:31 -0400

    Version 1.9.1

--- app/src/main/java/com/xda/nobar/views/BarView.kt
+++ app/src/main/java/com/xda/nobar/views/BarView.kt
@@ -12,7 +12,6 @@ import android.graphics.drawable.LayerDrawable
 import android.os.*
 import android.preference.PreferenceManager
 import android.util.AttributeSet
-import android.util.Log
 import android.view.*
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.DecelerateInterpolator
