deef54cb27884c6cd99a5f81eceb991a377262e6
zacharee <zachary.wander@gmail.com>
2019-10-24 16:28:27 -0400

    Use nav graph in MainActivity

--- app/src/main/java/com/xda/nobar/activities/MainActivity.kt
+++ app/src/main/java/com/xda/nobar/activities/MainActivity.kt
@@ -3,31 +3,19 @@ package com.xda.nobar.activities
 import android.animation.LayoutTransition
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.view.LayoutInflater
-import android.view.View
-import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.xda.nobar.R
-import com.xda.nobar.activities.ui.HelpAboutActivity
 import com.xda.nobar.activities.ui.IntroActivity
-import com.xda.nobar.activities.ui.SettingsActivity
-import com.xda.nobar.activities.ui.TroubleshootingActivity
-import com.xda.nobar.interfaces.OnGestureStateChangeListener
-import com.xda.nobar.interfaces.OnLicenseCheckResultListener
-import com.xda.nobar.interfaces.OnNavBarHideStateChangeListener
-import com.xda.nobar.util.*
-import com.xda.nobar.views.BarView
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * The main app activity
  */
-class MainActivity : AppCompatActivity(), OnGestureStateChangeListener, OnNavBarHideStateChangeListener, OnLicenseCheckResultListener {
+class MainActivity : AppCompatActivity() {
     companion object {
         fun start(context: Context) {
             context.startActivity(makeIntent(context))
@@ -40,12 +28,8 @@ class MainActivity : AppCompatActivity(), OnGestureStateChangeListener, OnNavBar
         }
     }
 
-    private val navListener = CompoundButton.OnCheckedChangeListener { _, isChecked ->
-        app.toggleNavState(!isChecked)
-        if (!hasWss) onNavStateChange(!isChecked)
-    }
-
-    private var currentPremReason: String? = null
+    private val navController: NavController
+        get() = findNavController(R.id.main_host)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,107 +39,10 @@ class MainActivity : AppCompatActivity(), OnGestureStateChangeListener, OnNavBar
         }
 
         setContentView(R.layout.activity_main)
-        setUpActionBar()
-
-        if (!app.rootWrapper.isConnected) app.rootWrapper.onCreate()
 
         root.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
 
-        app.addLicenseCheckListener(this)
-        app.addGestureActivationListener(this)
-        app.addNavBarHideListener(this)
-
-        activate.isChecked = prefManager.isActive
-        activate.onCheckedChangeListener = CompoundButton.OnCheckedChangeListener { button, isChecked ->
-            if ((Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this))
-                    && isAccessibilityEnabled) {
-                if (isChecked) app.addBar() else app.removeBar()
-                app.setGestureState(isChecked)
-            } else {
-                button.isChecked = false
-                IntroActivity.start(this)
-            }
-        }
-
-        hide_nav.onCheckedChangeListener = navListener
-
-        checkNavHiddenAsync {
-            onNavStateChange(it)
-        }
-
-        refresh_prem.setOnClickListener {
-            refresh()
-        }
-
-        prem_stat_clicker.setOnClickListener {
-            MaterialAlertDialogBuilder(this)
-                    .setMessage(currentPremReason)
-                    .show()
-        }
-
-        troubleshoot.setOnClickListener {
-            startActivity(Intent(this, TroubleshootingActivity::class.java))
-        }
-
-        refresh()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        if (prefManager.hideBetaPrompt) {
-            beta.visibility = View.GONE
-        } else {
-            beta.visibility = View.VISIBLE
-            beta.setOnClickListener {
-                MaterialAlertDialogBuilder(this)
-                        .setTitle(R.string.sign_up_for_beta)
-                        .setMessage(R.string.sign_up_for_beta_desc)
-                        .setPositiveButton(android.R.string.ok) { _, _ -> launchUrl("https://play.google.com/apps/testing/com.xda.nobar") }
-                        .setNegativeButton(android.R.string.cancel, null)
-                        .setNeutralButton(R.string.hide_text) { _,_ ->
-                            startActivity(Intent(this, HelpAboutActivity::class.java))
-                        }
-                        .show()
-            }
-        }
-    }
-
-    override fun onGestureStateChange(barView: BarView?, activated: Boolean) {
-        activate.isChecked = activated
-    }
-
-    override fun onNavStateChange(hidden: Boolean) {
-        hide_nav.onCheckedChangeListener = null
-        hide_nav.isChecked = hidden
-        hide_nav.onCheckedChangeListener = navListener
-    }
-
-    override fun onResult(valid: Boolean, reason: String?) {
-        currentPremReason = reason
-        runOnUiThread {
-            prem_stat.setTextColor(if (valid) Color.GREEN else Color.RED)
-            prem_stat.text = resources.getText(if (valid) R.string.installed else R.string.not_found)
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        app.removeLicenseCheckListener(this)
-        app.removeGestureActivationListener(this)
-        app.removeNavBarHideListener(this)
-
-        try {
-            app.removeGestureActivationListener(this)
-        } catch (e: Exception) {}
-    }
-
-    private fun refresh() {
-        prem_stat.setTextColor(Color.YELLOW)
-        prem_stat.text = resources.getText(R.string.checking)
-
-        app.refreshPremium()
+        setUpActionBar()
     }
 
     /**
@@ -165,10 +52,10 @@ class MainActivity : AppCompatActivity(), OnGestureStateChangeListener, OnNavBar
         setSupportActionBar(toolbar)
 
         val gear = LayoutInflater.from(this).inflate(R.layout.settings_button, toolbar, false)
-        gear.setOnClickListener { startActivity(Intent(this, SettingsActivity::class.java)) }
+        gear.setOnClickListener { navController.navigate(R.id.action_homeFragment_to_settingsActivity2) }
 
         val about = LayoutInflater.from(this).inflate(R.layout.help_button, toolbar, false)
-        about.setOnClickListener { startActivity(Intent(this, HelpAboutActivity::class.java)) }
+        about.setOnClickListener { navController.navigate(R.id.action_homeFragment_to_helpAboutActivity) }
 
         toolbar.addView(gear)
         toolbar.addView(about)
---
+++ app/src/main/java/com/xda/nobar/fragments/main/HomeFragment.kt
@@ -0,0 +1,138 @@
+package com.xda.nobar.fragments.main
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.CompoundButton
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.xda.nobar.App
+import com.xda.nobar.R
+import com.xda.nobar.activities.ui.IntroActivity
+import com.xda.nobar.activities.ui.TroubleshootingActivity
+import com.xda.nobar.interfaces.OnGestureStateChangeListener
+import com.xda.nobar.interfaces.OnLicenseCheckResultListener
+import com.xda.nobar.interfaces.OnNavBarHideStateChangeListener
+import com.xda.nobar.util.*
+import com.xda.nobar.views.BarView
+import kotlinx.android.synthetic.main.layout_main_activity.*
+import kotlinx.coroutines.launch
+
+class HomeFragment : Fragment(R.layout.layout_main_activity), OnGestureStateChangeListener,
+    OnNavBarHideStateChangeListener, OnLicenseCheckResultListener {
+    private val app: App
+        get() = requireContext().app
+    private val prefManager: PrefManager
+        get() = requireContext().prefManager
+
+    private val navListener = CompoundButton.OnCheckedChangeListener { _, isChecked ->
+        app.toggleNavState(!isChecked)
+        if (!requireContext().hasWss) onNavStateChange(!isChecked)
+    }
+
+    private var currentPremReason: String? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        if (!app.rootWrapper.isConnected) app.rootWrapper.onCreate()
+
+        app.addLicenseCheckListener(this)
+        app.addGestureActivationListener(this)
+        app.addNavBarHideListener(this)
+
+        activate.isChecked = prefManager.isActive
+        activate.onCheckedChangeListener = CompoundButton.OnCheckedChangeListener { button, isChecked ->
+            if ((Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(requireContext()))
+                && requireContext().isAccessibilityEnabled) {
+                if (isChecked) app.addBar() else app.removeBar()
+                app.setGestureState(isChecked)
+            } else {
+                button.isChecked = false
+                IntroActivity.start(requireContext())
+            }
+        }
+
+        hide_nav.onCheckedChangeListener = navListener
+
+        requireContext().checkNavHiddenAsync {
+            onNavStateChange(it)
+        }
+
+        refresh_prem.setOnClickListener {
+            refresh()
+        }
+
+        prem_stat_clicker.setOnClickListener {
+            MaterialAlertDialogBuilder(requireActivity())
+                .setMessage(currentPremReason)
+                .show()
+        }
+
+        troubleshoot.setOnClickListener {
+            startActivity(Intent(requireContext(), TroubleshootingActivity::class.java))
+        }
+
+        refresh()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (prefManager.hideBetaPrompt) {
+            beta.visibility = View.GONE
+        } else {
+            beta.visibility = View.VISIBLE
+            beta.setOnClickListener {
+                MaterialAlertDialogBuilder(requireActivity())
+                    .setTitle(R.string.sign_up_for_beta)
+                    .setMessage(R.string.sign_up_for_beta_desc)
+                    .setPositiveButton(android.R.string.ok) { _, _ -> context?.launchUrl("https://play.google.com/apps/testing/com.xda.nobar") }
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setNeutralButton(R.string.hide_text) { _,_ ->
+                        findNavController().navigate(R.id.action_homeFragment_to_helpAboutActivity)
+                    }
+                    .show()
+            }
+        }
+    }
+
+    override fun onGestureStateChange(barView: BarView?, activated: Boolean) {
+        activate.isChecked = activated
+    }
+
+    override fun onNavStateChange(hidden: Boolean) {
+        hide_nav.onCheckedChangeListener = null
+        hide_nav.isChecked = hidden
+        hide_nav.onCheckedChangeListener = navListener
+    }
+
+    override fun onResult(valid: Boolean, reason: String?) {
+        currentPremReason = reason
+        mainScope.launch {
+            prem_stat.setTextColor(if (valid) Color.GREEN else Color.RED)
+            prem_stat.text = resources.getText(if (valid) R.string.installed else R.string.not_found)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        app.removeLicenseCheckListener(this)
+        app.removeGestureActivationListener(this)
+        app.removeNavBarHideListener(this)
+
+        try {
+            app.removeGestureActivationListener(this)
+        } catch (e: Exception) {}
+    }
+
+    private fun refresh() {
+        prem_stat.setTextColor(Color.YELLOW)
+        prem_stat.text = resources.getText(R.string.checking)
+
+        app.refreshPremium()
+    }
+}
